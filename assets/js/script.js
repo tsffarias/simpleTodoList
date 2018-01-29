2ab3b7bb183443b58a2c96733ac8a619d81b3809
Thiago Silva Farias <tsffarias@gmail.com>
2018-01-29 16:15:07 -0200

    added font-awesome, google-fonts and styled a bit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,32 @@
 // check off specific todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
 // click on X to delete Todo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+// adding new Todo
+$("input[type='text']").keypress(function(event){
+	if (event.which === 13) {
+		// grabbing new todo text from input
+		var todoText = $(this).val();
+		//empting input text
+		$(this).val("");
+
+		//create new li and add to ul
+		$("ul").append("<li><span><i class=\"fa fa-trash\"></i></span> " + todoText + "</li>");
+
+
+	}
+});
+
+// toggling the form
+$(".fa-pencil-square-o").click(function(){
+	$("input[type='text']").fadeToggle();
 });
